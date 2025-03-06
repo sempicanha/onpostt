faad77b1fc2c3ec4cddc5b1bb62209b24ff97c97
Sem Picanha <roteiroal2025@gmail.com>
2025-03-06 03:57:16 -0300

    update file relay.js
    Melhoria no servidor WebSocket e validação de blocos
    
    Validação de Blocos: Melhorada a lógica de validação de blocos para garantir que os blocos sejam validados corretamente antes de serem salvos ou enviados.
    Gerenciamento de Conexões: Implementado o limite máximo de conexões simultâneas (MAX_CONNECTIONS) e timeout para inatividade (INACTIVITY_TIMEOUT).
    Processamento de Mensagens: Ajustado o processamento de mensagens, agora é feita a verificação do tamanho da mensagem para evitar sobrecarga do servidor (MAX_MESSAGE_SIZE).
    Broadcast para Todos os Clientes: Implementado o envio de blocos para todos os clientes conectados assim que um novo bloco for recebido e validado.
    Backup Automático: Criado processo de backup diário dos blocos em formato ZIP.
    Métricas de Monitoramento: Adicionada coleta de métricas do uso de memória e CPU, além do número de conexões ativas, a cada minuto.
    Correção de Erros: Melhor tratamento de erros, com logs detalhados e resposta apropriada para clientes (WebSocket).

--- relay.js
+++ relay.js
@@ -8,26 +8,37 @@ const ec = new EC('secp256k1');
 const crypto = require('crypto');
 const winston = require("winston");
 const archiver = require("archiver");
+const sanitizeHtml = require('sanitize-html');
 const os = require("os");
 dotenv.config();
+
 const app = express();
 const port = process.env.PORT || 3000;
-const blocksDir = path.join(__dirname, "blocks"); // Alterado de eventsDir para blocksDir
+const blocksDir = path.join(__dirname, "blocks");
 const backupDir = path.join(__dirname, "backups");
-if (!fs.existsSync(blocksDir)) {
-    fs.mkdirSync(blocksDir);
-}
-if (!fs.existsSync(backupDir)) {
-    fs.mkdirSync(backupDir);
-}
+
+// Importando as funções do arquivo filters.js
+const {
+    filterByPubkey,
+    filterByMode,
+    filterBySince,
+    filterByUntil,
+    filterByApp,
+    filterById,
+    filterByQuery,
+    filterByProfile
+} = require('./filters');
+
+// Configuração de diretórios
+if (!fs.existsSync(blocksDir)) fs.mkdirSync(blocksDir);
+if (!fs.existsSync(backupDir)) fs.mkdirSync(backupDir);
+
 const wss = new WebSocket.Server({ noServer: true });
 const clients = new Set();
-const MAX_BLOCKS_PER_REQUEST = parseInt(process.env.MAX_BLOCKS_PER_REQUEST) || 50; // Alterado de MAX_EVENTS_PER_REQUEST
+const MAX_BLOCKS_PER_REQUEST = parseInt(process.env.MAX_BLOCKS_PER_REQUEST) || 1000;
 const MAX_CONNECTIONS = parseInt(process.env.MAX_CONNECTIONS) || 100;
 const MAX_MESSAGE_SIZE = parseInt(process.env.MAX_MESSAGE_SIZE) || 1024 * 1024; // 1MB
-
-// Cache de blocos em memória
-const blockCache = new Map(); // Alterado de eventCache para blockCache
+const INACTIVITY_TIMEOUT = 600000; // 10 minuto de inatividade
 
 // Logger estruturado
 const logger = winston.createLogger({
@@ -39,52 +50,169 @@ const logger = winston.createLogger({
     ],
 });
 
-// Carrega blocos no cache ao iniciar o servidor
-function loadBlocksToCache() { // Alterado de loadEventsToCache
-    const files = fs.readdirSync(blocksDir); // Alterado de eventsDir
-    files.forEach((file) => {
-        const filePath = path.join(blocksDir, file); // Alterado de eventsDir
-        const block = JSON.parse(fs.readFileSync(filePath, 'utf8')); // Alterado de event para block
-        blockCache.set(block.id, block); // Alterado de eventCache para blockCache
-    });
-    logger.info(`Carregados ${blockCache.size} blocos no cache.`); // Alterado de eventos para blocos
-}
-loadBlocksToCache();
+// Validação de blocos
+function validateBlock(block) {
+    if (block.mode === "delete") {
+        return (
+            typeof block.id === "string" &&
+            typeof block.pubkey === "string" &&
+            typeof block.created_at === "number" &&
+            typeof block.mode === "string" &&
+            typeof block.blockId === "string" && // ID do bloco a ser deletado
+            typeof block.sig === "string"
+        );
+    }else if(block.mode === 'message'){
 
-function validateBlock(block) { // Alterado de validateEvent para validateBlock
-    return (
-        typeof block.id === "string" &&
-        typeof block.pubkey === "string" &&
-        typeof block.created_at === "number" &&
-        typeof block.mode === "string" &&
-        typeof block.content === "string" &&
-        typeof block.sig === "string" &&
-        typeof block.app === "string"
-    );
-}
+        return (
+            typeof block.id === "string" &&
+            typeof block.pubkey === "string" &&
+            typeof block.created_at === "number" &&
+            typeof block.mode === "string" &&
+            (typeof block.content === "string" || typeof block.content === "object") &&
+            typeof block.sig === "string" &&
+            typeof block.app === "string"
+        );
 
-// Função para salvar blocos
-function saveBlock(block) { // Alterado de saveEvent para saveBlock
-    try {
-        const fileName = `block_${block.pubkey}_${block.id}.json`;
-        const filePath = path.join(blocksDir, fileName); // Alterado de eventsDir para blocksDir
-        if (typeof block !== 'object') {
-            throw new Error('Bloco inválido'); // Alterado de Evento para Bloco
-        }
-        let formatBlock = { ...block }; // Alterado de event para block
-        const signature = formatBlock.sig;
-        delete formatBlock.sig;
-        if (!verifySignature(block.pubkey, JSON.stringify(formatBlock), signature)) {
-            logger.warn('❌ Assinatura inválida, bloco rejeitado!');
-            return;
-        }
-        fs.writeFileSync(filePath, JSON.stringify(block, null, 2));
-        blockCache.set(block.id, block); // Alterado de eventCache para blockCache
-        logger.info(`Bloco ${fileName} salvo com sucesso!`);
-    } catch (error) {
-        logger.error("Erro ao salvar bloco:", error.message); // Alterado de evento para bloco
+    }else {
+        return (
+            typeof block.id === "string" &&
+            typeof block.pubkey === "string" &&
+            typeof block.created_at === "number" &&
+            typeof block.mode === "string" &&
+            (typeof block.content === "string" || typeof block.content === "object") &&
+            typeof block.sig === "string" &&
+            typeof block.app === "string"
+        );
     }
 }
+
+const MAX_TIMESTAMP_DIFF = 60 * 60; // 1 hora
+
+function validateTimestamp(block) {
+    const currentTimestamp = Math.floor(Date.now() / 1000);
+    return (currentTimestamp - block.created_at) <= MAX_TIMESTAMP_DIFF;
+}
+
+
+// function sanitizeContent(content) {
+//     return sanitizeHtml(content, {
+//         allowedTags: [ 'b', 'i', 'em', 'strong', 'a', 'p', 'ul', 'ol', 'li', 'br', 'span' ], // tags permitidas
+//         allowedAttributes: {
+//             '*': ['class', 'style'], // Atributos permitidos para todas as tags
+//             'a': ['href', 'target'], // Atributos permitidos para a tag 'a'
+//         },
+//     });
+// }
+
+
+function saveBlock(block, ws) {
+    try {
+        const fileName = `block_${block.pubkey}_${block.id}.json`;
+        const filePath = path.join(blocksDir, fileName);
+
+        if (typeof block !== 'object') {
+            throw new Error('Bloco inválido');
+        }
+
+        // if (block.content) {
+        //     block.content = sanitizeContent(block.content); // Sanitiza o conteúdo HTML
+        // }
+
+
+        let formatBlock = { ...block };
+        const signature = formatBlock.sig;
+        delete formatBlock.sig;
+
+        if (!verifySignature(block.pubkey, JSON.stringify(formatBlock), signature)) {
+            logger.warn('❌ Assinatura inválida, bloco rejeitado!');
+            if (ws && ws.readyState === WebSocket.OPEN) {
+                ws.send(JSON.stringify({
+                    status: 'erro',
+                    message: 'Assinatura inválida. Bloco rejeitado.'
+                }));
+            }
+            return;
+        }
+
+        if (block.mode === "delete") {
+            const targetFilePath = path.join(blocksDir, `block_${block.pubkey}_${block.blockId}.json`);
+            if (!fs.existsSync(targetFilePath)) {
+                logger.warn(`❌ Bloco alvo (${block.blockId}) não encontrado.`);
+                if (ws && ws.readyState === WebSocket.OPEN) {
+                    ws.send(JSON.stringify({
+                        status: 'erro',
+                        message: `Bloco alvo (${block.blockId}) não encontrado.`
+                    }));
+                }
+                return;
+            }
+
+            const targetBlock = JSON.parse(fs.readFileSync(targetFilePath, 'utf8'));
+            if (targetBlock.pubkey !== block.pubkey) {
+                logger.warn(`❌ Tentativa de deletar bloco de outro autor.`);
+                if (ws && ws.readyState === WebSocket.OPEN) {
+                    ws.send(JSON.stringify({
+                        status: 'erro',
+                        message: `Tentativa de deletar bloco de outro autor.`
+                    }));
+                }
+                return;
+            }
+
+            targetBlock.deleted = true; // Marca o bloco como deletado
+            fs.writeFileSync(targetFilePath, JSON.stringify(targetBlock, null, 2));
+            logger.info(`✅ Bloco ${block.blockId} marcado como deletado.`);
+
+            if (ws && ws.readyState === WebSocket.OPEN) {
+                ws.send(JSON.stringify({
+                    status: 'success',
+                    message: `Bloco ${block.blockId} marcado como deletado.`
+                }));
+            }
+        } else {
+            // Tratando mensagem
+            if (block.mode === 'message') {
+                const isRecipient = block.query[1][0] === 'recipient';
+
+                if (isRecipient) {
+                    const recipientPubkey = block.query[1][1];
+                    // logger.info('Salvando para o receptor:', recipientPubkey);
+
+                    // Salva o bloco para o receptor
+                    const recipientFilePath = path.join(blocksDir, `block_${recipientPubkey}_${block.id}.json`);
+                    let recipientBlock = { ...block }; // Cria uma cópia do bloco para o receptor
+                    recipientBlock.pubkey = recipientPubkey; // Atualiza a pubkey para o recipient
+                    fs.writeFileSync(recipientFilePath, JSON.stringify(recipientBlock, null, 2));
+                    logger.info(`Bloco com recipient ${recipientPubkey} salvo com sucesso!`);
+                }
+
+                // Salva a cópia para o autor
+                fs.writeFileSync(filePath, JSON.stringify(block, null, 2));
+                logger.info(`Bloco com filePath salvo com sucesso!`);
+            } else {
+                // Caso não seja uma mensagem, salva normalmente
+                fs.writeFileSync(filePath, JSON.stringify(block, null, 2));
+                logger.info(`Bloco com filePath salvo com sucesso!`);
+            }
+
+            if (ws && ws.readyState === WebSocket.OPEN) {
+                ws.send(JSON.stringify({
+                    status: 'success',
+                    message: `Bloco ${block.id} publicado.`
+                }));
+            }
+        }
+    } catch (error) {
+        logger.error("Erro ao salvar bloco:", error.message);
+        if (ws && ws.readyState === WebSocket.OPEN) {
+            ws.send(JSON.stringify({
+                status: 'erro',
+                message: `Erro ao salvar bloco: ${error.message}`
+            }));
+        }
+    }
+}
+
 
 // Função para verificar assinaturas
 function verifySignature(publicKeyHex, message, signature) {
@@ -103,32 +231,50 @@ function sha256(message) {
     return crypto.createHash('sha256').update(message).digest('hex');
 }
 
-// Função para obter blocos filtrados
-function getBlocks(filter, callback) { // Alterado de getEvents para getBlocks
-    let results = Array.from(blockCache.values()).filter((block) => { // Alterado de eventCache para blockCache
-        let match = true;
-        if (filter.pubkey && block.pubkey !== filter.pubkey) match = false; // Alterado de event para block
-        if (filter.mode && block.mode !== filter.mode) match = false; // Alterado de event para block
-        if (filter.since && block.created_at < filter.since) match = false; // Alterado de event para block
-        if (filter.until && block.created_at > filter.until) match = false; // Alterado de event para block
-        if (filter.app && block.app !== filter.app) match = false; // Alterado de event para block
-        if (filter.query && Array.isArray(filter.query) && filter.query.length > 0) {
-            let parsedQuery = typeof block.query === 'string' ? JSON.parse(block.query) : block.query; // Alterado de event para block
-            filter.query.forEach(([key, value]) => {
-                let keyMatch = parsedQuery.some(([qKey, qValue]) => qKey === key && qValue === value);
-                if (!keyMatch) {
-                    match = false;
-                }
-            });
+function getBlocks(filter, callback) {
+    const files = fs.readdirSync(blocksDir);
+    let results = files.map(file => {
+        const filePath = path.join(blocksDir, file);
+        
+        try {
+            const data = fs.readFileSync(filePath, 'utf8');
+            return JSON.parse(data);
+        } catch (error) {
+            logger.error(`Erro ao processar ${file}: JSON inválido.`);
+            return null; // Retorna null para ser filtrado depois
         }
+
+    }).filter(block => block !== null) // Remove arquivos corrompidos
+    .filter(block => {
+        if (block.deleted) return false; // Ignora blocos deletados
+
+        // Aplica todos os filtros
+        let match = true;
+        match = match && filterByPubkey(block, filter.pubkey);
+        match = match && filterByMode(block, filter.mode);
+        match = match && filterBySince(block, filter.since);
+        match = match && filterByUntil(block, filter.until);
+        match = match && filterByApp(block, filter.app);
+        match = match && filterById(block, filter.id);
+        match = match && (filter.query ? filterByQuery(block, filter.query) : true); // Evita erro caso filter.query não exista
+
         return match;
     });
+
+    // Se o modo for "profile", manter apenas o mais recente por pubkey
+    if (filter.mode === "profile") {
+        results = filterByProfile(results);
+    }
+
+    // Ordena os resultados por timestamp (mais recente primeiro)
     results.sort((a, b) => b.created_at - a.created_at);
-    callback(results.slice(filter.offset || 0, (filter.offset || 0) + (filter.limit || MAX_BLOCKS_PER_REQUEST))); // Alterado de MAX_EVENTS_PER_REQUEST
+    callback(results.slice(filter.offset || 0, (filter.offset || 0) + (filter.limit || MAX_BLOCKS_PER_REQUEST)));
 }
 
+
+
 // Backup automático
-function backupBlocks() { // Alterado de backupEvents para backupBlocks
+function backupBlocks() {
     const timestamp = new Date().toISOString().replace(/[:.]/g, "-");
     const backupPath = path.join(backupDir, `backup_${timestamp}.zip`);
     const output = fs.createWriteStream(backupPath);
@@ -137,7 +283,7 @@ function backupBlocks() { // Alterado de backupEvents para backupBlocks
         logger.info(`Backup criado: ${archive.pointer()} bytes`);
     });
     archive.pipe(output);
-    archive.directory(blocksDir, false); // Alterado de eventsDir para blocksDir
+    archive.directory(blocksDir, false);
     archive.finalize();
 }
 setInterval(backupBlocks, 24 * 60 * 60 * 1000); // Backup diário
@@ -157,35 +303,102 @@ setInterval(() => {
     });
 }, 60000); // A cada minuto
 
-// WebSocket server
+// WebSocket server oficial
+// wss.on("connection", (ws) => {
+//     if (clients.size >= MAX_CONNECTIONS) {
+//         ws.close(1008, "Limite de conexões atingido");
+//         return;
+//     }
+//     clients.add(ws);
+
+//     // Fecha a conexão após um período de inatividade
+//     const inactivityTimeout = setTimeout(() => {
+//         ws.close(1000, "Conexão inativa");
+//     }, INACTIVITY_TIMEOUT);
+
+//     ws.on("message", (message) => {
+//         try {
+//             const data = JSON.parse(message);
+
+//             if (data.request === "get_blocks") {
+//                 getBlocks(data.filter || {}, (blocks) => {
+//                     ws.send(JSON.stringify({ response: "blocks", blocks, requestId: data.requestId }));
+//                 });
+//             } else if (validateBlock(data)) {
+//                 console.log('block', data);
+//                 saveBlock(data, ws);
+
+//                 // 🔥 **Aqui adicionamos logs para verificar se o broadcast está sendo feito** 🔥
+//                 console.log("📢 Broadcast para todos os clientes:", data);
+
+//                 clients.forEach((client) => {
+//                     if (client.readyState === WebSocket.OPEN) {
+//                         client.send(JSON.stringify(data));
+//                     }
+//                 });
+//             }else{
+//                 console.log('blo', data);
+//             }
+//         } catch (error) {
+//             console.error("Erro ao processar mensagem:", error.message);
+//         }
+//     });
+
+//     ws.on("close", () => {
+//         clients.delete(ws);
+//         clearTimeout(inactivityTimeout);
+//     });
+// });
+
 wss.on("connection", (ws) => {
     if (clients.size >= MAX_CONNECTIONS) {
         ws.close(1008, "Limite de conexões atingido");
         return;
     }
     clients.add(ws);
+
+    // Fecha a conexão após um período de inatividade
+    const inactivityTimeout = setTimeout(() => {
+        ws.close(1000, "Conexão inativa");
+    }, INACTIVITY_TIMEOUT);
+
     ws.on("message", (message) => {
-        if (message.length > MAX_MESSAGE_SIZE) {
-            ws.close(1009, "Mensagem muito grande");
-            return;
-        }
         try {
             const data = JSON.parse(message);
-            if (data.request === "get_blocks") { // Alterado de get_events para get_blocks
-                getBlocks(data.filter || {}, (blocks) => { // Alterado de getEvents para getBlocks
-                    ws.send(JSON.stringify({ response: "blocks", blocks, requestId: data.requestId })); // Alterado de events para blocks
+
+            if (message.length > MAX_MESSAGE_SIZE) {
+                ws.send(JSON.stringify({ status: 'erro', message: 'Mensagem muito grande' }));
+                return;
+            }
+
+            // Se for uma solicitação de blocos
+            if (data.request === "get_blocks") {
+                getBlocks(data.filter || {}, (blocks) => {
+                    ws.send(JSON.stringify({ response: "blocks", blocks, requestId: data.requestId }));
                 });
-            } else if (validateBlock(data)) { // Alterado de validateEvent para validateBlock
-                saveBlock(data); // Alterado de saveEvent para saveBlock
-            } else {
-                ws.send(JSON.stringify({ status: "erro", message: "Bloco inválido" })); // Alterado de Evento para Bloco
+            } 
+            // Se for um bloco válido, processa e salva
+            else if (validateBlock(data)) {
+                saveBlock(data, ws);
+
+                // 🔥 **Aqui fazemos o broadcast para todos os clientes** 🔥
+                logger.info("📢 Broadcast para todos os clientes:", data);
+
+                // Envia para todos os clientes conectados
+                clients.forEach((client) => {
+                    if (client.readyState === WebSocket.OPEN) {
+                        client.send(JSON.stringify(data));
+                    }
+                });
             }
         } catch (error) {
             logger.error("Erro ao processar mensagem:", error.message);
         }
     });
+
     ws.on("close", () => {
         clients.delete(ws);
+        clearTimeout(inactivityTimeout);
     });
 });
 
